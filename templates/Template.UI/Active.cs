552de825160953059a8ee3c028f38c69e4d7fb7d
Luc <lucvdijk@live.nl>
2025-05-08 14:44:37 +0200

    Adding Template.UI folder correctly

---
+++ templates/Template.UI/Active.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace UI
+{
+    public static class Active
+    {
+        public static Document Document 
+            => Application.DocumentManager.MdiActiveDocument;
+        
+        public static Editor Editor 
+            => Document.Editor;
+        
+        public static Database Database 
+            => Document.Database;
+        
+        public static void UsingTransaction(Action<Transaction>
+            action)
+        {
+            using (var transaction =
+                   Active.Database.TransactionManager.StartTransaction())
+            {
+                action(transaction);
+                transaction.Commit();
+            }
+        }
+    }
+}
